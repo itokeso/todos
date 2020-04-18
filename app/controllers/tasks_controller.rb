015ff66892f6251201f1d898daecca1cc8287cd9
itokeso <61177741+itokeso@users.noreply.github.com>
2020-04-18 23:21:41 +0900

    ajax

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,35 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task= Task.find(params[:id])
+    if @task.update(task_params)
+    redirect_to root_path
+    else
+    render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    if @task.destroy
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+  
+  def toggle
+    head :no_content
+    @task = Task.find(params[:id])
+    @task.done = !@task.done
+    @task.save
+  end
+
   private
   def task_params
   params.require(:task).permit(:title)
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,3 @@
 class Task < ApplicationRecord
-  validates :title ,presence: {message: "タイトルを入力してください"}
-  length:{ minimum: 5, message: "5文字以上入力してください"} 
+  validates :title ,presence: {message: "タイトルを入力してください"}, length: { minimum: 5, message: "5文字以上入力してください"} 
 end
